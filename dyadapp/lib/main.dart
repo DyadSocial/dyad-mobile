af4f598c1ea8898a1f0472d091d03bd24939b021
Vincent Pham <vpham@nevada.unr.edu>
2021-11-18 09:35:08 -0800

    feat(main.dart): add login page with no routes or input checking

--- dyadapp/lib/main.dart
+++ dyadapp/lib/main.dart
@@ -1,113 +1,149 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(DyadApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class DyadApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Welcome to Dyad',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blueGrey,
+          foregroundColor: Colors.orange,
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Login(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+// Login Info Class
+class LoginInfo {
+  String _username = '';
+  String _password = '';
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  LoginInfo();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  void setUsername(username) {
+    this._username = username;
+  }
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void setPassword(password) {
+    this._password = password;
   }
 
   @override
+  String toString() {
+    return "username: ${this._username}, password: ${this._password}";
+  }
+}
+
+// Login Widget
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+// Login State for Login
+class _LoginState extends State<Login> {
+  LoginInfo loginInfo = LoginInfo();
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Dyad'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.help),
+            onPressed: _pushLoginHelp,
+            tooltip: 'Login Help',
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: _buildLogin(),
+    );
+  }
+
+  void _pushLoginHelp() {}
+
+  Widget _buildLogin() {
+    return Center(
+      child: SizedBox(
+        width: 300,
+        child: ListView(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Container(
+              width: 500,
+              height: 125,
+              child: Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 48.0),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              height: 100,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.person),
+                  labelText: 'Phone Number',
+                ),
+                validator: (val) {
+                  if (val != null && val.isNotEmpty) {
+                    return null;
+                  } else if (val != null && val.length != 10) {
+                    return "Invalid phone number";
+                  }
+                },
+                keyboardType: TextInputType.phone,
+              ),
             ),
+            Container(
+              height: 80,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.lock),
+                  labelText: 'Password',
+                ),
+                validator: (val) {
+                  if (val != null && val.isNotEmpty) {
+                    return null;
+                  } else {
+                    return "Password cannot be empty";
+                  }
+                },
+                keyboardType: TextInputType.text,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(25, 20, 25, 20),
+              child: Container(
+                height: 40,
+                child: ElevatedButton(
+                  child: Text("Submit"),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 0, bottom: 0),
+              child: TextButton(
+                child: Text('Trouble signing in?'),
+                onPressed: () {},
+              ),
+            ),
+            TextButton(
+              child: Text('Create an account'),
+              onPressed: () {},
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
