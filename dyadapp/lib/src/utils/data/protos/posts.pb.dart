08365b7694329eb1eecf48a51f7507209008aaa9
Vincent Pham <vpham@nevada.unr.edu>
2022-02-03 11:13:23 -0800

    (utils/data/protos/*.pb.dart) format generated code

--- dyadapp/lib/src/utils/data/protos/posts.pb.dart
+++ dyadapp/lib/src/utils/data/protos/posts.pb.dart
@@ -13,14 +13,32 @@ import 'content.pb.dart' as $0;
 import 'google/protobuf/timestamp.pb.dart' as $1;
 
 class Post extends $pb.GeneratedMessage {
-  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Post', createEmptyInstance: create)
-    ..a<$core.int>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'id', $pb.PbFieldType.O3)
-    ..aOS(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'author')
-    ..aOM<$0.Content>(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'content', subBuilder: $0.Content.create)
-    ..aOM<$1.Timestamp>(4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'lastUpdated', subBuilder: $1.Timestamp.create)
-    ..aOM<$1.Timestamp>(5, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'created', subBuilder: $1.Timestamp.create)
-    ..hasRequiredFields = false
-  ;
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
+      const $core.bool.fromEnvironment('protobuf.omit_message_names')
+          ? ''
+          : 'Post',
+      createEmptyInstance: create)
+    ..a<$core.int>(
+        1,
+        const $core.bool.fromEnvironment('protobuf.omit_field_names')
+            ? ''
+            : 'id',
+        $pb.PbFieldType.O3)
+    ..aOS(
+        2,
+        const $core.bool.fromEnvironment('protobuf.omit_field_names')
+            ? ''
+            : 'author')
+    ..aOM<$0.Content>(
+        3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'content',
+        subBuilder: $0.Content.create)
+    ..aOM<$1.Timestamp>(
+        4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'lastUpdated',
+        subBuilder: $1.Timestamp.create)
+    ..aOM<$1.Timestamp>(5,
+        const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'created',
+        subBuilder: $1.Timestamp.create)
+    ..hasRequiredFields = false;
 
   Post._() : super();
   factory Post({
@@ -48,31 +66,39 @@ class Post extends $pb.GeneratedMessage {
     }
     return _result;
   }
-  factory Post.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
-  factory Post.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
-  @$core.Deprecated(
-  'Using this can add significant overhead to your binary. '
-  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
-  'Will be removed in next major version')
+  factory Post.fromBuffer($core.List<$core.int> i,
+          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromBuffer(i, r);
+  factory Post.fromJson($core.String i,
+          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromJson(i, r);
+  @$core.Deprecated('Using this can add significant overhead to your binary. '
+      'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+      'Will be removed in next major version')
   Post clone() => Post()..mergeFromMessage(this);
-  @$core.Deprecated(
-  'Using this can add significant overhead to your binary. '
-  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
-  'Will be removed in next major version')
-  Post copyWith(void Function(Post) updates) => super.copyWith((message) => updates(message as Post)) as Post; // ignore: deprecated_member_use
+  @$core.Deprecated('Using this can add significant overhead to your binary. '
+      'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+      'Will be removed in next major version')
+  Post copyWith(void Function(Post) updates) =>
+      super.copyWith((message) => updates(message as Post))
+          as Post; // ignore: deprecated_member_use
   $pb.BuilderInfo get info_ => _i;
   @$core.pragma('dart2js:noInline')
   static Post create() => Post._();
   Post createEmptyInstance() => create();
   static $pb.PbList<Post> createRepeated() => $pb.PbList<Post>();
   @$core.pragma('dart2js:noInline')
-  static Post getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Post>(create);
+  static Post getDefault() =>
+      _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Post>(create);
   static Post? _defaultInstance;
 
   @$pb.TagNumber(1)
   $core.int get id => $_getIZ(0);
   @$pb.TagNumber(1)
-  set id($core.int v) { $_setSignedInt32(0, v); }
+  set id($core.int v) {
+    $_setSignedInt32(0, v);
+  }
+
   @$pb.TagNumber(1)
   $core.bool hasId() => $_has(0);
   @$pb.TagNumber(1)
@@ -81,7 +107,10 @@ class Post extends $pb.GeneratedMessage {
   @$pb.TagNumber(2)
   $core.String get author => $_getSZ(1);
   @$pb.TagNumber(2)
-  set author($core.String v) { $_setString(1, v); }
+  set author($core.String v) {
+    $_setString(1, v);
+  }
+
   @$pb.TagNumber(2)
   $core.bool hasAuthor() => $_has(1);
   @$pb.TagNumber(2)
@@ -90,7 +119,10 @@ class Post extends $pb.GeneratedMessage {
   @$pb.TagNumber(3)
   $0.Content get content => $_getN(2);
   @$pb.TagNumber(3)
-  set content($0.Content v) { setField(3, v); }
+  set content($0.Content v) {
+    setField(3, v);
+  }
+
   @$pb.TagNumber(3)
   $core.bool hasContent() => $_has(2);
   @$pb.TagNumber(3)
@@ -101,7 +133,10 @@ class Post extends $pb.GeneratedMessage {
   @$pb.TagNumber(4)
   $1.Timestamp get lastUpdated => $_getN(3);
   @$pb.TagNumber(4)
-  set lastUpdated($1.Timestamp v) { setField(4, v); }
+  set lastUpdated($1.Timestamp v) {
+    setField(4, v);
+  }
+
   @$pb.TagNumber(4)
   $core.bool hasLastUpdated() => $_has(3);
   @$pb.TagNumber(4)
@@ -112,7 +147,10 @@ class Post extends $pb.GeneratedMessage {
   @$pb.TagNumber(5)
   $1.Timestamp get created => $_getN(4);
   @$pb.TagNumber(5)
-  set created($1.Timestamp v) { setField(5, v); }
+  set created($1.Timestamp v) {
+    setField(5, v);
+  }
+
   @$pb.TagNumber(5)
   $core.bool hasCreated() => $_has(4);
   @$pb.TagNumber(5)
@@ -122,12 +160,30 @@ class Post extends $pb.GeneratedMessage {
 }
 
 class Feed extends $pb.GeneratedMessage {
-  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Feed', createEmptyInstance: create)
-    ..pPS(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'recipients')
-    ..pc<Post>(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'posts', $pb.PbFieldType.PM, subBuilder: Post.create)
-    ..aOM<$1.Timestamp>(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'lastUpdated', subBuilder: $1.Timestamp.create)
-    ..hasRequiredFields = false
-  ;
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
+      const $core.bool.fromEnvironment('protobuf.omit_message_names')
+          ? ''
+          : 'Feed',
+      createEmptyInstance: create)
+    ..pPS(
+        1,
+        const $core.bool.fromEnvironment('protobuf.omit_field_names')
+            ? ''
+            : 'recipients')
+    ..pc<Post>(
+        2,
+        const $core.bool.fromEnvironment('protobuf.omit_field_names')
+            ? ''
+            : 'posts',
+        $pb.PbFieldType.PM,
+        subBuilder: Post.create)
+    ..aOM<$1.Timestamp>(
+        3,
+        const $core.bool.fromEnvironment('protobuf.omit_field_names')
+            ? ''
+            : 'lastUpdated',
+        subBuilder: $1.Timestamp.create)
+    ..hasRequiredFields = false;
 
   Feed._() : super();
   factory Feed({
@@ -147,25 +203,30 @@ class Feed extends $pb.GeneratedMessage {
     }
     return _result;
   }
-  factory Feed.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
-  factory Feed.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
-  @$core.Deprecated(
-  'Using this can add significant overhead to your binary. '
-  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
-  'Will be removed in next major version')
+  factory Feed.fromBuffer($core.List<$core.int> i,
+          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromBuffer(i, r);
+  factory Feed.fromJson($core.String i,
+          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromJson(i, r);
+  @$core.Deprecated('Using this can add significant overhead to your binary. '
+      'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+      'Will be removed in next major version')
   Feed clone() => Feed()..mergeFromMessage(this);
-  @$core.Deprecated(
-  'Using this can add significant overhead to your binary. '
-  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
-  'Will be removed in next major version')
-  Feed copyWith(void Function(Feed) updates) => super.copyWith((message) => updates(message as Feed)) as Feed; // ignore: deprecated_member_use
+  @$core.Deprecated('Using this can add significant overhead to your binary. '
+      'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+      'Will be removed in next major version')
+  Feed copyWith(void Function(Feed) updates) =>
+      super.copyWith((message) => updates(message as Feed))
+          as Feed; // ignore: deprecated_member_use
   $pb.BuilderInfo get info_ => _i;
   @$core.pragma('dart2js:noInline')
   static Feed create() => Feed._();
   Feed createEmptyInstance() => create();
   static $pb.PbList<Feed> createRepeated() => $pb.PbList<Feed>();
   @$core.pragma('dart2js:noInline')
-  static Feed getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Feed>(create);
+  static Feed getDefault() =>
+      _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Feed>(create);
   static Feed? _defaultInstance;
 
   @$pb.TagNumber(1)
@@ -177,7 +238,10 @@ class Feed extends $pb.GeneratedMessage {
   @$pb.TagNumber(3)
   $1.Timestamp get lastUpdated => $_getN(2);
   @$pb.TagNumber(3)
-  set lastUpdated($1.Timestamp v) { setField(3, v); }
+  set lastUpdated($1.Timestamp v) {
+    setField(3, v);
+  }
+
   @$pb.TagNumber(3)
   $core.bool hasLastUpdated() => $_has(2);
   @$pb.TagNumber(3)
@@ -185,4 +249,3 @@ class Feed extends $pb.GeneratedMessage {
   @$pb.TagNumber(3)
   $1.Timestamp ensureLastUpdated() => $_ensure(2);
 }
-
