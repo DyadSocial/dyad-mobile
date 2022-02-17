5815057270b2711fd5c11affa18e3d11e7690f19
Vincent Pham <vpham@nevada.unr.edu>
2022-02-16 22:37:11 -0800

    (network_handler.dart) rollback network_handler.dart

--- dyadapp/lib/src/utils/network_handler.dart
+++ dyadapp/lib/src/utils/network_handler.dart
@@ -1,106 +1,16 @@
-import 'dart:async';
-import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'package:device_info_plus/device_info_plus.dart';
-import 'package:flutter_nearby_connections/flutter_nearby_connections.dart';
-import 'package:flutter/material.dart';
+import 'package:http/testing.dart';
 
-enum ClientState {
-  advertising,
-  invisible,
-  off
+final http.Client client = MockClient(_handler);
+
+Future<http.Response> _handler(http.Request request) async {
+  var content = Map<String, dynamic>.from(
+      json.decode(request.body) as Map<String, dynamic>);
+
+  if (content['phone'] == '1234567890' && content['password'] == 'password')
+    return http.Response('', 200);
+
+  return http.Response('', 401);
 }
-
-class NetworkHandler extends ChangeNotifier {
-  final ClientState _clientState;
-
-  NetworkHandler(clientState) : this._clientState = clientState;
-
-  set connectedDevices(List<Device> devices) {
-    if (devices == _devices) return;
-    _devices = devices;
-    notifyListeners();
-  }
-
-  // Available hosts
-  List<Device> _devices = [];
-
-  // Connected hosts
-  List<Device> _connectedDevices = [];
-  late NearbyService nearbyService;
-
-  // asynchronous data transfer
-  late StreamSubscription subscription;
-  late StreamSubscription receievedDataSubscription;
-}
-
-/*
-  void init() async {
-    nearbyService = NearbyService();
-    String devInfo = '';
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    if (Platform.isAndroid) {
-      devInfo = (await deviceInfo.androidInfo).model as String;
-    } else if (Platform.isIOS) {
-      devInfo = (await deviceInfo.iosInfo).model as String;
-    }
-    await nearbyService.init(
-      serviceType: 'mpconn',
-      deviceName: devInfo,
-      strategy: Strategy.P2P_CLUSTER,
-      callback: (isRunning) async {
-        if (isRunning) {
-          if (clientState == ClientState.advertising) {
-            await nearbyService.stopAdvertisingPeer();
-            await nearbyService.stopBrowsingForPeers();
-            await Future.delayed(Duration(microseconds: 200));
-            await nearbyService.startBrowsingForPeers();
-            await nearbyService.startAdvertisingPeer();
-          } else if (clientState == ClientState.invisible) {
-            await nearbyService.stopBrowsingForPeers();
-            await Future.delayed(Duration(microseconds: 200));
-            await nearbyService.startBrowsingForPeers();
-          }
-        }
-      }
-    );
-    subscription = nearbyService.stateChangedSubscription(callback: (devicesList)
-    {
-      devicesList.forEach((element) {
-        print("deviceId: ${element.deviceId} | deviceName: ${element
-            .deviceName} | state: ${element.state}");
-
-        if (Platform.isAndroid) {
-          if (element.state == SessionState.connected) {
-            nearbyService.stopBrowsingForPeers();
-          } else {
-            nearbyService.startBrowsingForPeers();
-          }
-        }
-      });
-
-      setState(() {
-        devices.clear();
-        devices.addAll(devicesList);
-        connectedDevices.clear();
-        connectedDevices.addAll(devicesList
-          .where((device) => device.state == SessionState.connected)
-          .toList());
-      });
-    });
-
-    receivedDataSubscription = nearbyService.dataReceivedSubscription(callback: (data) {
-      print("dataReceivedSubscription: ${jsonEncode(data)}");
-      showToast(jsonEncode(data),
-          content: contex,
-          axis: Axis. horizontal,
-          alignment: Alignment.center,
-          position: StyledToastPosition.bottom);
-    });
-
-
-  }
-
-}
-*/
