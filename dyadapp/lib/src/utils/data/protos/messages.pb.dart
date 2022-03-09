a876e4130a604cf0a73829dc20789b0772a5cb2b
primw <prim@nevada.unr.edu>
2022-03-09 08:14:14 +0000

    inbox + message_page

--- dyadapp/lib/src/utils/data/protos/messages.pb.dart
+++ dyadapp/lib/src/utils/data/protos/messages.pb.dart
@@ -9,26 +9,27 @@ import 'dart:core' as $core;
 
 import 'package:protobuf/protobuf.dart' as $pb;
 
-import 'content.pb.dart' as $0;
-import 'google/protobuf/timestamp.pb.dart' as $1;
+import 'google/protobuf/timestamp.pb.dart' as $0;
 
 class Message extends $pb.GeneratedMessage {
   static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Message', createEmptyInstance: create)
-    ..a<$core.int>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'id', $pb.PbFieldType.O3)
+    ..aOS(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'id')
     ..aOS(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'author')
-    ..aOM<$0.Content>(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'content', subBuilder: $0.Content.create)
-    ..aOM<$1.Timestamp>(4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'lastUpdated', subBuilder: $1.Timestamp.create)
-    ..aOM<$1.Timestamp>(5, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'created', subBuilder: $1.Timestamp.create)
+    ..aOS(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'content')
+    ..aOM<$0.Timestamp>(4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'lastUpdated', subBuilder: $0.Timestamp.create)
+    ..aOM<$0.Timestamp>(5, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'created', subBuilder: $0.Timestamp.create)
+    ..aOB(6, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'image')
     ..hasRequiredFields = false
   ;
 
   Message._() : super();
   factory Message({
-    $core.int? id,
+    $core.String? id,
     $core.String? author,
-    $0.Content? content,
-    $1.Timestamp? lastUpdated,
-    $1.Timestamp? created,
+    $core.String? content,
+    $0.Timestamp? lastUpdated,
+    $0.Timestamp? created,
+    $core.bool? image,
   }) {
     final _result = create();
     if (id != null) {
@@ -45,6 +46,9 @@ class Message extends $pb.GeneratedMessage {
     }
     if (created != null) {
       _result.created = created;
+    }
+    if (image != null) {
+      _result.image = image;
     }
     return _result;
   }
@@ -70,9 +74,9 @@ class Message extends $pb.GeneratedMessage {
   static Message? _defaultInstance;
 
   @$pb.TagNumber(1)
-  $core.int get id => $_getIZ(0);
+  $core.String get id => $_getSZ(0);
   @$pb.TagNumber(1)
-  set id($core.int v) { $_setSignedInt32(0, v); }
+  set id($core.String v) { $_setString(0, v); }
   @$pb.TagNumber(1)
   $core.bool hasId() => $_has(0);
   @$pb.TagNumber(1)
@@ -88,44 +92,53 @@ class Message extends $pb.GeneratedMessage {
   void clearAuthor() => clearField(2);
 
   @$pb.TagNumber(3)
-  $0.Content get content => $_getN(2);
+  $core.String get content => $_getSZ(2);
   @$pb.TagNumber(3)
-  set content($0.Content v) { setField(3, v); }
+  set content($core.String v) { $_setString(2, v); }
   @$pb.TagNumber(3)
   $core.bool hasContent() => $_has(2);
   @$pb.TagNumber(3)
   void clearContent() => clearField(3);
-  @$pb.TagNumber(3)
-  $0.Content ensureContent() => $_ensure(2);
 
   @$pb.TagNumber(4)
-  $1.Timestamp get lastUpdated => $_getN(3);
+  $0.Timestamp get lastUpdated => $_getN(3);
   @$pb.TagNumber(4)
-  set lastUpdated($1.Timestamp v) { setField(4, v); }
+  set lastUpdated($0.Timestamp v) { setField(4, v); }
   @$pb.TagNumber(4)
   $core.bool hasLastUpdated() => $_has(3);
   @$pb.TagNumber(4)
   void clearLastUpdated() => clearField(4);
   @$pb.TagNumber(4)
-  $1.Timestamp ensureLastUpdated() => $_ensure(3);
+  $0.Timestamp ensureLastUpdated() => $_ensure(3);
 
   @$pb.TagNumber(5)
-  $1.Timestamp get created => $_getN(4);
+  $0.Timestamp get created => $_getN(4);
   @$pb.TagNumber(5)
-  set created($1.Timestamp v) { setField(5, v); }
+  set created($0.Timestamp v) { setField(5, v); }
   @$pb.TagNumber(5)
   $core.bool hasCreated() => $_has(4);
   @$pb.TagNumber(5)
   void clearCreated() => clearField(5);
   @$pb.TagNumber(5)
-  $1.Timestamp ensureCreated() => $_ensure(4);
+  $0.Timestamp ensureCreated() => $_ensure(4);
+
+  @$pb.TagNumber(6)
+  $core.bool get image => $_getBF(5);
+  @$pb.TagNumber(6)
+  set image($core.bool v) { $_setBool(5, v); }
+  @$pb.TagNumber(6)
+  $core.bool hasImage() => $_has(5);
+  @$pb.TagNumber(6)
+  void clearImage() => clearField(6);
 }
 
 class Chat extends $pb.GeneratedMessage {
   static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Chat', createEmptyInstance: create)
     ..pPS(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'recipients')
     ..pc<Message>(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'messages', $pb.PbFieldType.PM, subBuilder: Message.create)
-    ..aOM<$1.Timestamp>(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'lastUpdated', subBuilder: $1.Timestamp.create)
+    ..aOM<$0.Timestamp>(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'lastUpdated', subBuilder: $0.Timestamp.create)
+    ..aOS(4, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'id')
+    ..aOS(5, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'owner')
     ..hasRequiredFields = false
   ;
 
@@ -133,7 +146,9 @@ class Chat extends $pb.GeneratedMessage {
   factory Chat({
     $core.Iterable<$core.String>? recipients,
     $core.Iterable<Message>? messages,
-    $1.Timestamp? lastUpdated,
+    $0.Timestamp? lastUpdated,
+    $core.String? id,
+    $core.String? owner,
   }) {
     final _result = create();
     if (recipients != null) {
@@ -144,6 +159,12 @@ class Chat extends $pb.GeneratedMessage {
     }
     if (lastUpdated != null) {
       _result.lastUpdated = lastUpdated;
+    }
+    if (id != null) {
+      _result.id = id;
+    }
+    if (owner != null) {
+      _result.owner = owner;
     }
     return _result;
   }
@@ -175,14 +196,32 @@ class Chat extends $pb.GeneratedMessage {
   $core.List<Message> get messages => $_getList(1);
 
   @$pb.TagNumber(3)
-  $1.Timestamp get lastUpdated => $_getN(2);
+  $0.Timestamp get lastUpdated => $_getN(2);
   @$pb.TagNumber(3)
-  set lastUpdated($1.Timestamp v) { setField(3, v); }
+  set lastUpdated($0.Timestamp v) { setField(3, v); }
   @$pb.TagNumber(3)
   $core.bool hasLastUpdated() => $_has(2);
   @$pb.TagNumber(3)
   void clearLastUpdated() => clearField(3);
   @$pb.TagNumber(3)
-  $1.Timestamp ensureLastUpdated() => $_ensure(2);
+  $0.Timestamp ensureLastUpdated() => $_ensure(2);
+
+  @$pb.TagNumber(4)
+  $core.String get id => $_getSZ(3);
+  @$pb.TagNumber(4)
+  set id($core.String v) { $_setString(3, v); }
+  @$pb.TagNumber(4)
+  $core.bool hasId() => $_has(3);
+  @$pb.TagNumber(4)
+  void clearId() => clearField(4);
+
+  @$pb.TagNumber(5)
+  $core.String get owner => $_getSZ(4);
+  @$pb.TagNumber(5)
+  set owner($core.String v) { $_setString(4, v); }
+  @$pb.TagNumber(5)
+  $core.bool hasOwner() => $_has(4);
+  @$pb.TagNumber(5)
+  void clearOwner() => clearField(5);
 }
 
