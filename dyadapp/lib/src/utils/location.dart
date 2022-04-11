63fb65ffd6c30cc3a4054738eac1217d2586fd1b
Jake D <jakob.delossantos1@gmail.com>
2022-04-11 16:14:44 -0700

    Fixed location.dart to use try gps and catch geoip

--- dyadapp/lib/src/utils/location.dart
+++ dyadapp/lib/src/utils/location.dart
@@ -9,37 +9,39 @@ class LocationDyad {
   static var currentAddress;
   static var latitude;
   static var longitude;
-  static var enabled;
-  getUserPosition() async {
-    //If location services are enabled, use method _getCurrentLocation to get what to display on the map
-    final Position currentPosition = await _getCurrentLocation();
-    //To be used in the map screen
-    if(enabled == true){
-      try{
-        List<Placemark> placemarks = await placemarkFromCoordinates(
-            currentPosition.latitude, currentPosition.longitude);
-        Placemark place = placemarks[0];
-        if (place.locality != null) {
-          currentAddress = place.locality;
-        }
-        latitude = currentPosition.latitude;
-        longitude = currentPosition.longitude;
-      }
-      catch(e) {
-        //Must be on windows or if user accepts location but the api not working, or turned off gps
-        final Map<String, dynamic> temp = await fetchLocationByIp();
-        if (temp != {}) {
-          currentAddress = temp['city'];
-          latitude = temp['lat'];
-          longitude = temp['lon'];
-        }
-      }
 
+  getUserPosition() async {
+    try{
+      LocationPermission permission = await Geolocator.checkPermission();
+      bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      //Location off
+      if (!serviceEnabled) {
+        throw Exception('Location services are disabled.');
+      }
+      permission = await Geolocator.requestPermission();
+      //Check again
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permissions are denied.');
+      }
+      //Always denied
+      if (permission == LocationPermission.deniedForever) {
+        throw Exception(
+            'Location permissions are permanently denied, we cannot request permissions. Will instead use Geo-IP which is public.');
+      }
+      //All permissions good, get location
+      final Position currentPosition = await Geolocator.getCurrentPosition();
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          currentPosition.latitude, currentPosition.longitude);
+      Placemark place = placemarks[0];
+      if (place.locality != null) {
+        currentAddress = place.locality;
+      }
+      latitude = currentPosition.latitude;
+      longitude = currentPosition.longitude;
     }
-    //If location is disabled, use method fetchLocationByIp to get what to display on map. Uses the public geo-ip instead.
-    else{
+    catch(e){
       final Map<String, dynamic> temp = await fetchLocationByIp();
-      if(temp != {}){
+      if (temp != {}) {
         currentAddress = temp['city'];
         latitude = temp['lat'];
         longitude = temp['lon'];
@@ -50,35 +52,6 @@ class LocationDyad {
         longitude = 25.0;
       }
     }
-  }
-
-  Future<Position> _getCurrentLocation() async {
-    LocationPermission permission = await Geolocator.checkPermission();
-    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    //Location off
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
-    }
-    //Permission denied once
-    if (permission == LocationPermission.denied) {
-      enabled = false;
-      permission = await Geolocator.requestPermission();
-      //Check again
-      if (permission == LocationPermission.denied) {
-        enabled = false;
-        return Future.error('Location permissions are denied.');
-      }
-    }
-    //Always denied
-    if (permission == LocationPermission.deniedForever) {
-      enabled = false;
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions. Will instead use Geo-IP which is public.');
-    }
-
-    //All permissions good, get location
-    enabled = true;
-    return await Geolocator.getCurrentPosition();
   }
 
   Future<Map<String, dynamic>> fetchLocationByIp() async{
